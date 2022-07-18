9ebdd2664fe1db0dd461dacf3f9d0647936c5401
antonyg5 <104701006+antonyg5@users.noreply.github.com>
2022-07-18 14:48:49 -0300

    Update destaques_page.dart

--- lib/pages/destaques_page.dart
+++ lib/pages/destaques_page.dart
@@ -1,3 +1,4 @@
+import 'package:antony_913/data/bd.dart';
 import 'package:antony_913/pages/pacote_promocional.dart';
 import 'package:flutter/material.dart';
 import '../domain/pacote_turistico.dart';
@@ -11,116 +12,24 @@ class DestaquesPage extends StatefulWidget {
 }
 
 class _DestaquesPageState extends State<DestaquesPage> {
-  List<PacoteTuristico>? lista;
 
-  Future<List<PacoteTuristico>> getValues() async {
-    await Future.delayed(const Duration(seconds: 10));
-    lista = [
-      PacoteTuristico(
-        imagem:
-        'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-        titulo: 'Pacote Cancún 2022/2023',
-        transporte: 'Aéreo - Hotel All inclusive',
-        cidade: 'CANCÚN, MEX',
-        validade: 'De 01 ago 2022 até 30 set 2023',
-        numDiarias: 5,
-        numPessoas: 2,
-        numParcelas: 6,
-        desconto: 45,
-        precoAntigo: 6819,
-        precoAtual: 2819,
-      ),
-      PacoteTuristico(
-        imagem:
-        'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
-        titulo: 'Pacote Maragogi 2023',
-        transporte: 'Hotel All inclusive',
-        cidade: 'MARAGOGI, BRA',
-        validade: 'De 01 ago 2022 até 30 set 2023',
-        numDiarias: 7,
-        numPessoas: 3,
-        numParcelas: 12,
-        desconto: 30,
-        precoAntigo: 4819,
-        precoAtual: 819,
-      ),
-      PacoteTuristico(
-        imagem:
-        'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
-        titulo: 'Pacote Maragogi 2023',
-        transporte: 'Hotel All inclusive',
-        cidade: 'MARAGOGI, BRA',
-        validade: 'De 01 ago 2022 até 30 set 2023',
-        numDiarias: 7,
-        numPessoas: 3,
-        numParcelas: 12,
-        desconto: 30,
-        precoAntigo: 4819,
-        precoAtual: 819,
-      ),
-      PacoteTuristico(
-        imagem:
-        'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
-        titulo: 'Pacote Maragogi 2023',
-        transporte: 'Hotel All inclusive',
-        cidade: 'MARAGOGI, BRA',
-        validade: 'De 01 ago 2022 até 30 set 2023',
-        numDiarias: 7,
-        numPessoas: 3,
-        numParcelas: 12,
-        desconto: 30,
-        precoAntigo: 4819,
-        precoAtual: 819,
-      ),
-      PacoteTuristico(
-        imagem:
-        'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
-        titulo: 'Pacote Maragogi 2023',
-        transporte: 'Hotel All inclusive',
-        cidade: 'MARAGOGI, BRA',
-        validade: 'De 01 ago 2022 até 30 set 2023',
-        numDiarias: 7,
-        numPessoas: 3,
-        numParcelas: 12,
-        desconto: 30,
-        precoAntigo: 4819,
-        precoAtual: 819,
-      ),
-      PacoteTuristico(
-        imagem:
-        'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
-        titulo: 'Pacote Maragogi 2023',
-        transporte: 'Hotel All inclusive',
-        cidade: 'MARAGOGI, BRA',
-        validade: 'De 01 ago 2022 até 30 set 2023',
-        numDiarias: 7,
-        numPessoas: 3,
-        numParcelas: 12,
-        desconto: 30,
-        precoAntigo: 4819,
-        precoAtual: 819,
-      ),
-    ];
-    return lista ?? [];
-  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: FutureBuilder<List<PacoteTuristico>>(
-        future: getValues(),
+        future: BD.getValues(),
         builder: (context, snapshot) {
 
           if (snapshot.hasData) {
-            List<PacoteTuristico> l = snapshot.data ?? [];
+            List<PacoteTuristico> lista = snapshot.data ?? [];
 
             return ListView(
               children: [
-                buildContainerPropaganda(context),
+                buildContainerPropaganda(),
                 const SizedBox(height: 16),
-                CardPacoteTuristico(pacoteTuristico: l[0]),
-                CardPacoteTuristico(pacoteTuristico: l[1]),
+                buildListViewBuilder(lista),
               ],
             );
           }
@@ -130,14 +39,26 @@ class _DestaquesPageState extends State<DestaquesPage> {
       ),
     );
   }
+  buildListViewBuilder(List<PacoteTuristico> listaPacotes) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: listaPacotes.length,
+      itemBuilder: (context, index) {
+        return CardPacoteTuristico(
+          pacoteTuristico: listaPacotes[index],
+        );
+      },
+    );
 
-  buildContainerPropaganda(BuildContext context) {
+  }
+  buildContainerPropaganda() {
     return Container(
       decoration: BoxDecoration(
         color: Colors.purple,
         borderRadius: BorderRadius.circular(16),
       ),
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Row(
         children: [
           Expanded(
@@ -171,7 +92,7 @@ class _DestaquesPageState extends State<DestaquesPage> {
                     'Eu quero',
                     style: TextStyle(color: Colors.black),
                   ),
-                  style: ElevatedButton.styleFrom(primary: Color(0xFFF8FF04)),
+                  style: ElevatedButton.styleFrom(primary: const Color(0xFFF8FF04)),
                 )
               ],
             ),
