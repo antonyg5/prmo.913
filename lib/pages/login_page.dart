b997eee9829d239218848a8627bbb7a0c87d6e2a
antonyg5 <104701006+antonyg5@users.noreply.github.com>
2022-08-09 09:38:13 -0300

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
-import 'package:antony_913/pages/home_page.dart';
-import 'package:antony_913/widget/custom_snackbar.dart';
 import 'package:flutter/material.dart';
+import '/pages/cadastro_page.dart';
+import '/pages/turmas_page.dart';
+
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,79 +11,174 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _showPassword = false;
   final _formKey = GlobalKey<FormState>();
-  TextEditingController userController = TextEditingController();
-  TextEditingController pswdController = TextEditingController();
+  TextEditingController usercontroller = TextEditingController();
+  TextEditingController pswdcontroller = TextEditingController();
+
+
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: buildBody(),
-      ),
-    );
-  }
-
-   buildBody(){
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: SingleChildScrollView(
-        child: Form(
-
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children:  [
-              const Placeholder(fallbackHeight: 150,),
-              const SizedBox(height: 32,),
-              TextFormField(
-                controller: userController,
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return 'Campo de E-mail obrigatório';
-                  }
-                  return null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'E-mail',
-                  prefixIcon: Icon(Icons.mail_outline_outlined, size: 20, color: Colors.grey,),
-                ),
-              ),
-              const SizedBox(height: 32,),
-              TextFormField(
-                controller: pswdController,
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return 'Campo de Senha obrigatório';
-                  } else if (value.length < 8) {
-                    return 'Senha deve conter 8 dígitos';
-                  }
-                  return null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Senha',
-                  prefixIcon: Icon(Icons.lock_outline, size: 20, color: Colors.grey,),
-                ),
-              ),
-              const SizedBox(height: 32,),
-              ElevatedButton(onPressed: onPressed,
-                  style: ElevatedButton.styleFrom(primary: const Color(0xFFE81F7C)),
-                  child:
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12),
-                    child:  Text(
-                      'Entrar com a conta Hurb',
-                      style: TextStyle(
-                      fontSize: 20,
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(24),
+        color: const Color(0xFF6BC07D),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 24.0),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                    Image.network(
+                      'https://www2.ifal.edu.br/o-ifal/comunicacao/arquivos/logos/copy_of_IFALvertical.png',
+                      height: 145,
+                      width: 145,
                     ),
+                    const SizedBox(
+                      height: 16,
                     ),
-                  ),
-              ),
-            ],
+                    const Center(
+                      child: Text(
+                        'SUPORTE PARA MONITORIA',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          fontFamily: 'Roboto',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 52),
+                     Padding(
+                        padding: const EdgeInsets.only(
+                            left: 16.0, right: 16.0, bottom: 2.0),
+                        child: TextFormField(
+                          controller: usercontroller,
+                          validator: (value){
+                            if(value == null || value.isEmpty){
+                              return 'Campo de E-mail obrigatório';
+                            }
+                            return null;
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          decoration:  InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: 'E-mail',
+                            prefixIcon: Icon(Icons.mail_outline, color: Colors.grey,),
+                            filled: true,
+                            fillColor: Colors.white,
+                          ),
+                        ),
+                      ),
+                    const SizedBox(height: 40),
+                       Padding(
+                        padding: const EdgeInsets.only(
+                            left: 16.0, right: 16.0, bottom: 2.0),
+                        child: TextFormField(
+                          controller: pswdcontroller,
+                          validator: (value){
+                            if(value == null || value.isEmpty){
+                              return 'Campo de senha obrigatório';
+                            }
+                            return null;
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          decoration:  InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: 'Senha',
+                            prefixIcon: Icon(Icons.lock_outline, color: Colors.grey,),
+                            filled: true,
+                            fillColor: Colors.white,
+                            suffixIcon: GestureDetector(
+                              child: Icon(_showPassword == false ? Icons.visibility_off : Icons.visibility, color: Colors.grey,),
+                            onTap: () {
+                                setState(() {
+                                  _showPassword = !_showPassword;
+                                });
+                            },
+                            ),
+                          ),
+                          obscureText: _showPassword == false ? true : false,
+                        ),
+                      ),
+
+                    const SizedBox(height: 6),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Text(
+                          'Esqueci a senha',
+                          style: TextStyle(
+                            color: Color(0xFFB22222),
+                            fontSize: 18,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 36),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32), // <-- Radius
+                        ),
+                      ),
+                      onPressed: onPressed,
+                      child: const Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            color: Color(0xFF6BC07D),
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:  [
+                        const Text(
+                          'Não tem uma conta?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(
+                                    builder: (context){
+                                      return CadastroPage();
+                                    }
+                                    )
+                            );
+                          },
+                          child:
+                          const Text("Cadastre-se",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+              ],
+            ),
           ),
         ),
       ),
@@ -91,8 +187,8 @@ class _LoginPageState extends State<LoginPage> {
 
   void onPressed(){
     if(_formKey.currentState!.validate()) {
-      String userDigitado = userController.text;
-      String pswdDigitado = pswdController.text;
+      String userDigitado = usercontroller.text;
+      String pswdDigitado = pswdcontroller.text;
 
       String user = "asa@gmail.com";
       String senha = "asagigante";
@@ -102,19 +198,15 @@ class _LoginPageState extends State<LoginPage> {
           context,
           MaterialPageRoute(
             builder: (context) {
-              return const HomePage();
+              return const TurmasPage();
             },
           ),
         );
-        final snack = Utils.customSnackBar(context, 'Login correto');
 
-        ScaffoldMessenger.of(context).showSnackBar(snack);
-      } else {
-        final snack = Utils.customSnackBar(
-            context, 'Erro na validação');
+      }else{
+        const snack = SnackBar(content: Text('Login Incorreto'));
         ScaffoldMessenger.of(context).showSnackBar(snack);
       }
+      }
     }
-
-  }
 }
